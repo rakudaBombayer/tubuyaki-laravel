fa02918f38a4754a3e7db50019b106f3d930d320
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-06-21 17:19:27 +0900

    p59

--- database/seeders/TweetsSeeder.php
+++ database/seeders/TweetsSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class TweetsSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class TweetsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('tweets')->insert([
+            'content' => Str::random(100),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
